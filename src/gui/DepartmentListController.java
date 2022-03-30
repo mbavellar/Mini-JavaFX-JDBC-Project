4fb0a8fa5dbfb2f5b3b2ce43d06919411cac44c5
Mauricio Avellar <86550730+mbavellar@users.noreply.github.com>
2022-03-30 13:33:03 -0300

    Department Form

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,17 +1,26 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 
@@ -30,8 +39,8 @@ public class DepartmentListController implements Initializable {
   private Button btnNewDepartment;
 
   @FXML
-  public void onBtnNewDepartment() {
-
+  public void onBtnNewDepartment(ActionEvent e) {
+    createDialogForm("/gui/DepartmentForm.fxml", Utils.currentStage(e));
   }
 
   @Override
@@ -50,5 +59,20 @@ public class DepartmentListController implements Initializable {
   
   public void updateTableView(List<Department> departments) {
     tableViewDepartment.setItems(FXCollections.observableArrayList(departments));
+  }
+  
+  private void createDialogForm(String absoluteName, Stage parentStage) {
+    try {
+      Pane pane = FXMLLoader.load(getClass().getResource(absoluteName));
+      Stage departmentFormStage = new Stage();
+      departmentFormStage.setTitle("Enter Department Data:");
+      departmentFormStage.setScene(new Scene(pane));
+      departmentFormStage.setResizable(false);
+      departmentFormStage.initOwner(parentStage);
+      departmentFormStage.initModality(Modality.WINDOW_MODAL);
+      departmentFormStage.showAndWait();
+    } catch (IOException e) {
+      Alerts.showAlert("ERROR", "Could NOT load Add Department!", e.getMessage(), AlertType.ERROR);
+    }
   }
 }
